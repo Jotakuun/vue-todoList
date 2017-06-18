b21520635649dc1ee76951356153d6d01040cb40
Juanma Rivas <jrg@aquilessolutions.com>
2017-06-18 22:02:43 +0200

    add new task simple functionality

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -3,12 +3,24 @@
 
 <template>
   <div>
-    <input type="text" name="newtask"></input>
+    <input type="text" name="newtask"
+    v-bind:value="value"
+    v-on:keyup.enter="addTask"
+    v-on:input="updateValue($event.target.value)"></input>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'new-task'
+  name: 'new-task',
+  props: ['value'],
+  methods: {
+    updateValue: function(value) {
+      this.$emit('input', value);
+    },
+    addTask: function(){
+      this.$emit('addtask');
+    }
+  }
 }
 </script>
--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>This is the list of task</h2>
-    <new-task></new-task>
+    <new-task v-model="newtask" v-on:addtask="createNewTask()"></new-task>
     <div v-for="task in tasks">
       <task :task="task"></task>
     </div>
@@ -10,7 +10,6 @@
 
 
 <style lang="sass">
-    
 </style>
 
 <script>
@@ -23,13 +22,19 @@ export default {
     'task' : task,
     'new-task' : newTask
   },
-  data: function (){
+  data: function() {
     return {
       newtask: '',
       tasks: [
         {title: 'Learn Javascript', complete: false},
         {title: 'Do something else', complete: false}
       ]
+    }
+  },
+  methods: {
+    createNewTask: function() {
+      this.tasks.push({title: this.newtask, complete: false});
+      this.newtask = '';      
     }
   }
 }
