67edb9bd0ed0d14238d0e19dc965ee0343194aca
Juanma Rivas <jotakuun@gmail.com>
2017-07-16 19:53:40 +0200

    :+1:

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,12 +1,14 @@
 <template>
   <li class="Task">
-    <input :id="'Task'+index" type="checkbox" class="Task__Checkbox"></input>
-    <label :for="'Task'+index" class="Task__Label">
-      <svg width="20px" height="20px" viewBox="0 0 20 20">
-      <path d="M3,1 L17,1 L17,1 C18.1045695,1 19,1.8954305 19,3 L19,17 L19,17 C19,18.1045695 18.1045695,19 17,19 L3,19 L3,19 C1.8954305,19 1,18.1045695 1,17 L1,3 L1,3 C1,1.8954305 1.8954305,1 3,1 Z"></path>
-      <polyline points="4 11 8 15 16 6"></polyline>
-    </svg>
-    </label>
+    <div class="Task__Check">
+      <input :id="'Task'+index" type="checkbox" class="Task__Checkbox"></input>
+      <label :for="'Task'+index" class="Task__Label">
+        <svg width="20px" height="20px" viewBox="0 0 20 20">
+          <path d="M3,1 L17,1 L17,1 C18.1045695,1 19,1.8954305 19,3 L19,17 L19,17 C19,18.1045695 18.1045695,19 17,19 L3,19 L3,19 C1.8954305,19 1,18.1045695 1,17 L1,3 L1,3 C1,1.8954305 1.8954305,1 3,1 Z"></path>
+          <polyline points="4 11 8 15 16 6"></polyline>
+        </svg>
+      </label>
+    </div>
     <span>{{task.title}}</span>
     <icon name="remove_circle" class="Task__RemoveIcon" :scale="100"></icon>
   </li>
